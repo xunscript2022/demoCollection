e4b1e25131e8a0702b7534c8ee8aba674fed31a5
xunscript2022 <xunscript@163.com>
2022-05-08 23:58:21 +0800

    watermark-createItem

--- packages/d031_watermark/index.js
+++ packages/d031_watermark/index.js
@@ -1,36 +1,77 @@
 window.onload = function (){
     setTimeout(()=>{
         console.log('水印执行了');
-        embedWatermark();
+        embedWatermark('demo');
     },500)
 }
 
-const wrapStyle = {
+const watermarkStyle = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+}
+
+const itemStyle = {
     position: 'absolute',
-    top: '50%',
-    left: '20%',
-    fontSize: '32px',
+    top: '200px',
+    left: '50px',
+    fontSize: '14px',
     color: '#000',
     lineHeight: 1.5,
     opacity: 0.4,
     transform: `rotate(-15deg)`,
     transformOrigin: '0 0',
+    //使文字无法被使用
     userSelect: 'none',
     whiteSpace: 'nowrap',
     overflow: 'hidden',
+    //使元素实体虚化，不触发鼠标事件
+    //pointervents: none
 } 
 
-function cssHelper(el, wrapStyle) {
-    for (let i in wrapStyle) {
-        el.style[i] = wrapStyle[i]
+function cssHelper(el, domStyle) {
+    for (let i in domStyle) {
+        el.style[i] = domStyle[i]
     }
 }
 
-function embedWatermark(username){
-    const wrapDom = document.createElement('div');
-    wrapDom.innerHTML = `${username || window.username} ${new Date()}`;
-    cssHelper(wrapDom, wrapStyle);
-    document.body.appendChild(wrapDom);
+function createItem(username){
+    const itemDom = document.createElement('div');
+    itemDom.innerHTML = `${username || window.username} ${new Date()}`;
+    cssHelper(itemDom, itemStyle);
+    return itemDom;
 }
+
+
+function embedWatermark(username){
+    const waterHeight = 400;
+    const waterWidth = 150;
+    const { clientWidth, clientHeight } = document.documentElement || document.body;
+    const column = Math.ceil(clientWidth / waterWidth);
+    const rows = Math.ceil(clientHeight / waterHeight);
+    const watermarkDom = document.createElement('div');
+    cssHelper(watermarkDom, watermarkStyle);
+
+    const wrapStyle = {
+        position: 'relative',
+        width: `500px`,
+        height: `300px`,
+        // flex: `0 0 500px`,
+        overflow: 'hidden',
+    }
+
+    for (let i = 0; i < column * rows; i++) {
+        const wrap = document.createElement('div');
+        cssHelper(wrap, wrapStyle);
+        wrap.appendChild(createItem(username));
+        watermarkDom.appendChild(wrap)
+    }
+    document.body.appendChild(watermarkDom)
+    
+}
+
+
 
 
